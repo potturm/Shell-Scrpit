f74a6bfe605dd2ba9871834c794475caaeedb8b2
10669270 <devops.aws042023@gmail.com>
2023-08-21 12:40:38 +0530

    Added variables|MMM-01

--- 02-Variables.sh
+++ 02-Variables.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-#Variables and Value form script
+# 1 .Variables and Value form script
 per1=MOUNi
 per2=JOLI
 echo "$per1 Good Morning $per2"
+# 2. How to execute the command inside the shellscript and take the ouput 
+DA=$(date)
+echo "timestamp now is :$DA"
